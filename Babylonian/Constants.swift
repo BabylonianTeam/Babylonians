2dd7acf474b99900ffbd7a67a8ce6ca6ff54db5e
Ben Freeman <djregn@gmail.com>
2016-03-14 16:35:03 -0500

    Add constants file with a global reference to the root firebase url

---
+++ Babylonian/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Ben Freeman on 3/14/16.
+//
+//
+
+import Foundation
+import Firebase
+
+let FIREBASE_REF = Firebase(url: "https://babylonians.firebaseio.com")
