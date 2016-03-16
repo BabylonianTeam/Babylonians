820e2ebe882e6bcb75507344de38c74b800fe914
Eric <8BitRain@gmail.com>
2016-03-16 11:11:38 -0500

    Updating MyCourses

---
+++ Babylonian/Babylonian/CourseCell.swift
@@ -0,0 +1,14 @@
+//
+//  CourseCell.swift
+//  Babylonian
+//
+//  Created by Eric Smith on 3/14/16.
+//  Copyright © 2016 Eric Smith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CourseCell : UITableViewCell{
+  
+}
